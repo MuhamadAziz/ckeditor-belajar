12faae4e5a60643e1b606b1587a29c59c2ca79b6
MuhamadAziz <azis49146@gmail.com>
2018-12-10 16:39:36 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CKEditor from './ckeditor.js'
+
+class App extends Component {
+  render() {
+    return (
+      <CKEditor/>
+    );
+  }
+}
+
+export default App;
