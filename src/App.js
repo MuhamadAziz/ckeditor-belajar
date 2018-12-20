b985890463048fc0133c0c4ba3998e167f50ccdf
MuhamadAziz <azis49146@gmail.com>
2018-12-20 13:36:53 +0700

    hapus2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import CKEditor from 'react-ckeditor-component/lib/ckeditor.js'
 // import CKEditor from './ckeditor.js'
-// 
+//
 class App extends Component {
   render() {
     return (
